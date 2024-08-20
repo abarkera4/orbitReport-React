5db7513d2289219387d10e0480b8ba9c07816267
abarkera4 <abarkera4@gmail.com>
2024-08-20 15:03:52 -0500

    Further attempt at gh-pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(() => {
-  return {
-    build: {
-      outDir: "build",
-    },
-    test: {
-      include: ["**/*.test.jsx"],
-      globals: true,
-      environment: "jsdom",
-      setupFiles: "src/setupTests.js",
-    },
-    plugins: [react()],
-    base: "/orbitReport-React/",
-  };
+export default defineConfig({
+  plugins: [react()],
+  base: "/orbitReport-React/",
+  build: {
+    outDir: "build", // Ensuring build output is in the "build" directory
+  },
+  test: {
+    include: ["**/*.test.jsx"],
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "src/setupTests.js",
+  },
 });
