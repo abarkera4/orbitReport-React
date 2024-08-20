e69bd9ec9be15f32e3e06ebafe0572da392d609a
abarkera4 <abarkera4@gmail.com>
2024-08-20 14:54:43 -0500

    Fixed Banner Styling

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig(() => {
   return {
     build: {
-      outDir: 'build',
+      outDir: "build",
     },
     test: {
-      include: ['**/*.test.jsx'],
+      include: ["**/*.test.jsx"],
       globals: true,
-      environment: 'jsdom',
-      setupFiles: 'src/setupTests.js'
+      environment: "jsdom",
+      setupFiles: "src/setupTests.js",
     },
     plugins: [react()],
+    base: "/orbitReport-React/",
   };
 });
